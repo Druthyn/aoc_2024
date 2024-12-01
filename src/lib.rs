cf41d45ac479d367e147efaeebc0b2f9bcf53e38
Jadzia Read <jadzia.read@clearspace.today>
2024-12-01 21:21:13 +0000

    Refactor for cleanliness

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
-
-pub fn split_file_into_int_lists(input: &str) -> (Vec<u32>, Vec<u32>) {
-    let parsed_input = input.split_ascii_whitespace()
-                                                        .map(|x| x.parse().unwrap());
-    let left: Vec<u32> = parsed_input.clone()
-                                            .step_by(2)
-                                            .collect();
-    let right: Vec<u32> = parsed_input.skip(1)
-                                            .step_by(2)
-                                            .collect();
-    (left, right)
-}
