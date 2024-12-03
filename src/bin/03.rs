5919061aabc88f5eab4e9f86b33fc2f309054f74
Jadzia Read <jadzia.read@clearspace.today>
2024-12-03 22:30:23 +0000

    Day 3 rough

---
+++ src/bin/03.rs
@@ -0,0 +1,67 @@
+advent_of_code::solution!(3);
+
+use regex::Regex;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    Some(
+        re.find_iter(input)
+            .map(|x| {
+                x.as_str()[4..x.len() - 1]
+                    .split(',')
+                    .map(|x| x.parse::<u32>().unwrap())
+                    .product::<u32>()
+            })
+            .sum(),
+    )
+    // Some(
+    //     re.captures_iter(input)
+    //         .map(|c| c.extract())
+    //         .map(|(_, [x, y])| (x.parse().unwrap(), y.parse().unwrap()))
+    //         .map(|(x, y): (u32, u32)| x * y)
+    //         .sum(),
+    // )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)()()|don't\(\)()()").unwrap();
+
+    let mut flag: bool = true;
+    let mut sum = 0;
+    for x in re.find_iter(input).map(|x| x.as_str()) {
+        match &x[0..3] {
+            "mul" => {
+                if flag {
+                    sum += x[4..x.len() - 1]
+                        .split(',')
+                        .map(|x| x.parse::<u32>().unwrap())
+                        .product::<u32>();
+                }
+            }
+            "do(" => flag = true,
+            "don" => flag = false,
+            _ => panic!(),
+        }
+    }
+    Some(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(161));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(48));
+    }
+}
