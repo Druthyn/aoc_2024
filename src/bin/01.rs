d240a8faf792a51f7832bede444d099d154ae793
Jadzia Read <jadzia.read@clearspace.today>
2024-12-08 16:35:19 +0000

    Coorect day 1's "optimisations"

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -2,21 +2,28 @@ advent_of_code::solution!(1);
 
 use std::collections::HashMap;
 
+fn split_file_into_int_lists(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let parsed_input = input.split_ascii_whitespace().map(|x| x.parse().unwrap());
+    let left: Vec<u32> = parsed_input.clone().step_by(2).collect();
+    let right: Vec<u32> = parsed_input.skip(1).step_by(2).collect();
+    (left, right)
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let (mut left, mut right) = split_file_into_int_lists(input);
+    left.sort();
+    right.sort();
+
     Some(
-        input
-            .lines()
-            .map(|s| s.split("   ").map(|s| s.parse::<u32>().unwrap()))
-            .map(|mut e| e.next().unwrap().abs_diff(e.next().unwrap()))
+        left.into_iter()
+            .zip(right)
+            .map(|(l, r)| (l).abs_diff(r))
             .sum(),
     )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let parsed_input = input.split_ascii_whitespace().map(|x| x.parse().unwrap());
-    let left: Vec<u32> = parsed_input.clone().step_by(2).collect();
-    let right: Vec<u32> = parsed_input.skip(1).step_by(2).collect();
-
+    let (left, right) = split_file_into_int_lists(input);
     let mut right_buckets = HashMap::new();
 
     for v in right {
