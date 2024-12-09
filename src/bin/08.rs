8fbb8b17532ab37e54230ff0ee57e06e24d899dd
Jadzia Read <jadzia.read@clearspace.today>
2024-12-09 00:16:09 +0000

    Refactor day 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -16,14 +16,6 @@ impl Position {
     }
 }
 
-impl std::ops::Deref for Position {
-    type Target = Position;
-
-    fn deref(&self) -> &Self::Target {
-        self
-    }
-}
-
 impl std::ops::Add for Position {
     type Output = Position;
 
@@ -59,95 +51,73 @@ fn parse_input(input: &str) -> (AntennaMap, (usize, usize)) {
     (out, dims)
 }
 
-fn get_antinodes_part_1(a: &Position, b: &Position, limit: &(usize, usize)) -> Option<Vec<Position>> {
+fn get_antinodes_part(a: &Position, b: &Position, limit: &(usize, usize), is_part_2: bool) -> Option<Vec<Position>> {
     fn is_valid(pos: Position, limit: &(usize, usize)) -> bool {
         !(pos.x < 0 || pos.y < 0 || pos.x >= limit.0 as isize || pos.y >= limit.1 as isize)
     }
     let delta = *a - *b;
-    let anti_a = *a + delta;
-    let anti_b = *b - delta;
-
     let mut out = vec!{};
-    if is_valid(anti_a, limit) {
-        out.push(anti_a);
+
+    if is_part_2 {
+        let mut anti_a = *a;
+        let mut anti_b = *b;
+
+        while is_valid(anti_a, limit) {
+            out.push(anti_a);
+            anti_a = anti_a + delta;
+        }
+
+        while is_valid(anti_b, limit) {
+            out.push(anti_b);
+            anti_b = anti_b - delta;
+        }
+    } else {
+        let anti_a = *a + delta;
+        let anti_b = *b - delta;
+
+        if is_valid(anti_a, limit) {
+            out.push(anti_a);
+        }
+
+        if is_valid(anti_b, limit) {
+            out.push(anti_b);
+        }
     }
 
-    if is_valid(anti_b, limit) {
-        out.push(anti_b);
-    }
     if out.is_empty() {
         return None;
     }
     Some(out)
 }
 
-fn get_antinodes_part_2(a: &Position, b: &Position, limit: &(usize, usize)) -> Option<Vec<Position>> {
-    fn is_valid(pos: Position, limit: &(usize, usize)) -> bool {
-        !(pos.x < 0 || pos.y < 0 || pos.x >= limit.0 as isize || pos.y >= limit.1 as isize)
-    }
-    let delta = *a - *b;
-    let mut anti_a = *a + delta;
-    let mut anti_b = *b - delta;
+fn solve(input: &str, is_part_two: bool) -> Option<u32> {
+    let (stations, limits) = parse_input(input);
 
-    let mut out = vec!{};
-    while is_valid(anti_a, limit) {
-        out.push(anti_a);
-        anti_a = anti_a + delta;
-    }
+    let mut antinodes = HashSet::new();
 
-    while is_valid(anti_b, limit) {
-        out.push(anti_b);
-        anti_b = anti_b + delta;
+    for station_type in stations.keys() {
+        for station1 in stations.get(station_type).unwrap() {
+            for station2 in stations.get(station_type).unwrap() {
+                if station1 != station2 {
+                    if let Some(v) =  get_antinodes_part(station1, station2, &limits, is_part_two) {
+                        for antinode in v {
+                            antinodes.insert(antinode);
+                        }
+                    }
+
+                }
+            }
+        }
     }
-    if out.is_empty() {
-        return None;
-    }
-    Some(out)
+    Some(antinodes.len() as u32)
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (stations, limits) = parse_input(input);
-
-    let mut antinodes = HashSet::new();
-
-    for station_type in stations.keys() {
-        for station1 in stations.get(station_type).unwrap() {
-            for station2 in stations.get(station_type).unwrap() {
-                if station1 != station2 {
-                    if let Some(v) =  get_antinodes_part_1(station1, station2, &limits) {
-                        for antinode in v {
-                            antinodes.insert(antinode);
-                        }
-                    }
-
-                }
-            }
-        }
-    }
-    Some(antinodes.len() as u32)
+    solve(input, false)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (stations, limits) = parse_input(input);
-
-    let mut antinodes = HashSet::new();
-
-    for station_type in stations.keys() {
-        for station1 in stations.get(station_type).unwrap() {
-            antinodes.insert(*station1);
-            for station2 in stations.get(station_type).unwrap() {
-                if station1 != station2 {
-                    if let Some(v) =  get_antinodes_part_2(station1, station2, &limits) {
-                        for antinode in v {
-                            antinodes.insert(antinode);
-                        }
-                    }
-
-                }
-            }
-        }
-    }
-    Some(antinodes.len() as u32)
+    solve(input, true)
 }
 
 #[cfg(test)]
@@ -164,5 +134,19 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(34));
+    }
+
+    #[test]
+    fn answer_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        let answer = advent_of_code::template::read_file_part("answers", DAY, 1).parse().unwrap();
+        assert_eq!(result, Some(answer));
+    }
+
+    #[test]
+    fn answer_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        let answer = advent_of_code::template::read_file_part("answers", DAY, 2).parse().unwrap();
+        assert_eq!(result, Some(answer));
     }
 }
