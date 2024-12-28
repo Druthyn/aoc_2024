dbddf37c5019d6b835778d0256b652eb86eb70df
Jadzia Read <jadzia.read@clearspace.today>
2024-12-28 15:21:33 +0000

    Change of laptop wips commit

---
+++ src/bin/16.rs
@@ -0,0 +1,34 @@
+use advent_of_code::grid::Grid;
+
+advent_of_code::solution!(16);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let map = Grid::new(input.lines().map(|l| l.chars().collect()).collect());
+    let start = (1_usize, map.len() - 2);
+    let end = (map[0].len() - 2, 1_usize);
+
+    
+
+    None
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        assert_eq!(result, Some(7036));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, Some(11048));
+    }
+}
