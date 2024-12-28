31f6902dcebfbeb0d5bc22330386037297e914e5
Jadzia -ubuntu laptop <jadzia_ubuntu@laptop.rock>
2024-12-28 23:11:54 +0000

    Cargo fmt results

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -4,10 +4,8 @@ advent_of_code::solution!(16);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let map = Grid::new(input.lines().map(|l| l.chars().collect()).collect());
-    let start = (1_usize, map.len() - 2);
-    let end = (map[0].len() - 2, 1_usize);
-
-    
+    let _start = (1_usize, map.len() - 2);
+    let _end = (map[0].len() - 2, 1_usize);
 
     None
 }
@@ -22,13 +20,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(7036));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(11048));
     }
 }
